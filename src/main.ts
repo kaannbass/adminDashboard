9bd86c0b495161934be49037db1d7bdb23bb32ce
kaan.bas <baskaan8@gmail.com>
2023-04-14 13:24:46 +0300

    Added flowbite axios pinia

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import '@/css/index.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import '@/css/index.css';
+import axios from 'axios';
+import { createPinia } from 'pinia';
 
 const app = createApp(App);
+const pinia = createPinia()
 
 app.use(store);
 app.use(router);
+app.use(pinia);
+
+
+app.config.globalProperties.axios = axios;
+
+
 app.mount('#app');
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  content: [],
+  purge: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    require("flowbite/plugin"),
+  ],
+  content: ["./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
